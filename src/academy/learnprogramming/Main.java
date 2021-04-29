588d9eaad85038e01173d081af29662bfed2737d
Richard <richard.yang.tong@gmail.com>
2021-04-29 09:05:51 -0400

    Added char type and corresponding unicode symbol.

--- src/academy/learnprogramming/Main.java
+++ src/academy/learnprogramming/Main.java
@@ -3,6 +3,10 @@ package academy.learnprogramming;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        char myChar = 'D';
+        char myUnicodeChar = '\u0044';
+        System.out.println("myChar is " + myChar);
+        System.out.println("myUnicodeChar is " + myUnicodeChar);
     }
 }
